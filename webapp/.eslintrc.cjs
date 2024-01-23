a7c37ea175c628ec2ecf5c0b7e3dac97cc668d6f
savsalmon <savannah.b.hilton@gmail.com>
2024-01-22 20:48:46 -0700

    commiting ui to vercel hopefully. env still broken

--- webapp/.eslintrc.cjs
+++ webapp/.eslintrc.cjs
@@ -2,8 +2,15 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
+  globals: {
+    process: true
+  },
+  env: {
+    browser: true,
+    node: true
+  },
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,26 +1,24 @@
 <script>
-import fetch from 'node-fetch'
 
-const url = 'https://api.themoviedb.org/3/configuration';
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: "./.env.MOVIE_AUTH_CODE"
-  }
-};
-
-fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .catch(err => console.error('error:' + err));
 </script>
 
 <template>
-  <header class="text-red-700">
-    <h1 class="text-red-700">Vue 3 + Vite + Tailwind CSS</h1>
-  </header>
+  <div id="wrapper" class="flex flex-col">
+    <header class="flex flex-row m-5 gap-5">
+    <a href="https://www.savcode.co/"><img src="/images/SAVWhiteLogo.png" alt="sav logo" class="md:h-8 w-auto h-4"></a>
+    <h1 class="text-xl font-light">Your Movie Search</h1>
+    </header>
 
-  <main>
+  <main class="flex flex-col items-center gap-5">
+    <h1 class="text-4xl uppercase font-light">Find Your Movie.</h1>
+    <div class="flex flex-row gap-3">
+    <input type="text" placeholder="Type here" class="input input-bordered input-primary w-full max-w-sm" />
+    <button class="btn btn-primary">Search</button>
+  </div>
+  <!--Movie Display below-->
+  <div id="movieDisplay">
+
+  </div>
   </main>
+  </div>
 </template>
